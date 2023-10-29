03c9572f986f395326ce86822baf5295b4e33147
j-mahapatra <jagannathm.official@gmail.com>
2023-10-29 21:07:13 +0530

    feat: Setup Uploadthing

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,28 @@
+import { auth } from '@clerk/nextjs';
+import { createUploadthing, type FileRouter } from 'uploadthing/next';
+
+const f = createUploadthing();
+
+const handleAuth = () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    throw new Error('Unauthorized');
+  }
+
+  return { userId };
+};
+
+export const ourFileRouter = {
+  courseImage: f({ image: { maxFileCount: 1, maxFileSize: '4MB' } })
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+  courseAttachment: f(['text', 'image', 'video', 'audio', 'pdf'])
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+  chapterVideo: f({ video: { maxFileCount: 1, maxFileSize: '1GB' } })
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
