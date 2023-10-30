f7c37fa529aa7ba941ed0db145461ed398494b47
j-mahapatra <jagannathm.official@gmail.com>
2023-10-30 19:51:01 +0530

    chore: add script to populate categories in db

---
+++ scripts/seed.ts
@@ -0,0 +1,28 @@
+const { PrismaClient } = require('@prisma/client');
+
+const database = new PrismaClient();
+
+async function main() {
+  try {
+    await database.category.createMany({
+      data: [
+        { name: 'Business' },
+        { name: 'Computer Science' },
+        { name: 'Design' },
+        { name: 'Health & Fitness' },
+        { name: 'Lifestyle' },
+        { name: 'Marketing' },
+        { name: 'Music' },
+        { name: 'Soft Skill' },
+        { name: 'Photography & Video' },
+      ],
+    });
+    console.log('Seeded the database categories successfully.');
+  } catch (error) {
+    console.error('Error seeding the database categories', error);
+  } finally {
+    await database.$disconnect();
+  }
+}
+
+main();
