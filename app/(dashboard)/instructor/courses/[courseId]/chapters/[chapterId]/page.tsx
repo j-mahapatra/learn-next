80fbe5bed65c059abaa6040d4c4338f7037d8573
j-mahapatra <jagannathm.official@gmail.com>
2023-11-03 12:12:47 +0530

    feat: create chapter setup page
    - Created chapter setup page.
    - Created a ChapterTitleForm component and added it to this setup page.
    - Created an API route to update chapter title.

---
+++ app/(dashboard)/instructor/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -0,0 +1,79 @@
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+import { db } from '@/lib/database';
+import { auth } from '@clerk/nextjs';
+import { ArrowBigLeft, PencilRuler } from 'lucide-react';
+import { IconBadge } from '@/components/ui/icon-badge';
+
+import ChapterTitleForm from '@/components/chapter-title-form';
+
+const ChapterPage = async ({
+  params,
+}: {
+  params: { courseId: string; chapterId: string };
+}) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect('/');
+  }
+
+  const chapter = await db.chapter.findUnique({
+    where: {
+      id: params.chapterId,
+      courseId: params.courseId,
+    },
+    include: {
+      muxData: true,
+    },
+  });
+
+  if (!chapter) {
+    return redirect('/');
+  }
+
+  const requiredFields = [chapter.title, chapter.description, chapter.videoUrl];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  return (
+    <div className='p-4'>
+      <div className='flex justify-between items-center'>
+        <div className='w-full mr-auto'>
+          <Link
+            className='flex items-center justify-left text-sm hover:text-ring tansition mb-6'
+            href={`/instructor/courses/${params.courseId}`}
+          >
+            <ArrowBigLeft /> Back to Course Setup
+          </Link>
+          <div className='flex justify-between items-center w-full'>
+            <div className='flex flex-col gap-y-2'>
+              <h1 className='text-2xl font-bold'>Chapter Setup</h1>
+              <p className='text-sm'>
+                Complete all fields {`(${completedFields}/${totalFields})`}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-5 mt-10'>
+        <div className='space-y-4'>
+          <div>
+            <div className='flex items-center gap-x-2'>
+              <IconBadge icon={PencilRuler} />
+              <h2 className='text-xl px-1'>Customize Your Chapter</h2>
+            </div>
+            <ChapterTitleForm
+              courseId={params.courseId}
+              initialData={chapter}
+              chapterId={params.chapterId}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChapterPage;
