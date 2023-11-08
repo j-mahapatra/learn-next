14b98889bdfa75636b6c14817d12e8abf0eb8743
j-mahapatra <jagannathm.official@gmail.com>
2023-11-09 03:41:10 +0530

    feat: add course list to student search page
    - Created a CoursesList component that shows all the published courses
      in student search page.
    - Created CourseCard component that will show course details in
      CourseList component.
    - Created actions to get course data and progress data from db.

--- app/(dashboard)/search/page.tsx
+++ app/(dashboard)/search/page.tsx
@@ -1,13 +1,37 @@
+import { redirect } from 'next/navigation';
+import { auth } from '@clerk/nextjs';
+
 import { db } from '@/lib/database';
+import { getCourses } from '@/actions/get-courses';
 import Categories from '@/components/categories';
 import SearchInput from '@/components/search-input';
+import CoursesList from '@/components/courses-list';
 
-export default async function SearchPage() {
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+export default async function SearchPage({ searchParams }: SearchPageProps) {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect('/');
+  }
+
   const categories = await db.category.findMany({
     orderBy: {
       name: 'asc',
     },
   });
+
+  const courses = await getCourses({
+    userId,
+    ...searchParams,
+  });
+
   return (
     <>
       <div className='px-5 pt-5 block md:hidden md:mb-0'>
@@ -15,6 +39,7 @@ export default async function SearchPage() {
       </div>
       <div className='p-5'>
         <Categories items={categories} />
+        <CoursesList items={courses} />
       </div>
     </>
   );
---
+++ components/courses-list/partials/course-card.tsx
@@ -0,0 +1,63 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { BookOpenText } from 'lucide-react';
+
+import { IconBadge } from '@/components/ui/icon-badge';
+// import { CourseProgress } from '@/components/course-progress';
+
+interface CourseCardProps {
+  id: string;
+  title: string;
+  imageUrl: string;
+  chaptersLength: number;
+  price: number;
+  progress: number | null;
+  category: string;
+}
+
+export const CourseCard = ({
+  id,
+  title,
+  imageUrl,
+  chaptersLength,
+  price,
+  progress,
+  category,
+}: CourseCardProps) => {
+  const formattedPrice = (price: number) => {
+    return new Intl.NumberFormat('en-IN', {
+      style: 'currency',
+      currency: 'INR',
+    }).format(price);
+  };
+  return (
+    <Link href={`/courses/${id}`}>
+      <div className='group hover:shadow-sm transition overflow-hidden border rounded-md p-4 h-full'>
+        <div className='relative w-full aspect-video rounded-md overflow-hidden'>
+          <Image fill className='object-cover' alt={title} src={imageUrl} />
+        </div>
+        <div className='flex flex-col pt-2'>
+          <div className='text-lg md:text-base font-medium group-hover:text-ring transition line-clamp-2'>
+            {title}
+          </div>
+          <p className='text-xs text-muted-foreground'>{category}</p>
+          <div className='my-3 flex items-center gap-x-2 text-sm md:text-xs'>
+            <div className='flex items-center gap-x-1'>
+              <IconBadge size='sm' icon={BookOpenText} />
+              <p>
+                {chaptersLength} {chaptersLength === 1 ? 'Chapter' : 'Chapters'}
+              </p>
+            </div>
+          </div>
+          {progress !== null ? (
+            <>{/* <div>TODO: Course Progress</div> */}</>
+          ) : (
+            <p className='text-md md:text-sm font-medium'>
+              {formattedPrice(price)}
+            </p>
+          )}
+        </div>
+      </div>
+    </Link>
+  );
+};
