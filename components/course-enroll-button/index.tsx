c57c93f7b25daf23ef0d69fd3f6c528c9eef3cc3
j-mahapatra <jagannathm.official@gmail.com>
2023-11-10 05:03:04 +0530

    feat: add stripe payment functionality
    - Created a checkout API route that creates a stripe customer in
      database and generates a stripe session.
    - Created a webhook API route to handle stripe actions.
    - Updated the CourseEnrollButton component to initiate payment when
      clicked.
    - Updated clerkJS middleware to allow /api/webhook route.

--- components/course-enroll-button/index.tsx
+++ components/course-enroll-button/index.tsx
@@ -1,4 +1,10 @@
+'use client';
+
+import { useState } from 'react';
+
 import { Button } from '@/components/ui/button';
+import toast from 'react-hot-toast';
+import axios from 'axios';
 
 const CourseEnrollButton = ({
   courseId,
@@ -7,14 +13,34 @@ const CourseEnrollButton = ({
   courseId: string;
   price: number;
 }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.post(`/api/courses/${courseId}/checkout`);
+      window.location.assign(response.data.url);
+    } catch (error) {
+      toast.error('Something went wrong!');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   const formattedPrice = (price: number) => {
     return new Intl.NumberFormat('en-IN', {
       style: 'currency',
       currency: 'INR',
     }).format(price);
   };
+
   return (
-    <Button variant='secondary' size='sm' className='w-full md:w-auto'>
+    <Button
+      variant='secondary'
+      size='sm'
+      className='w-full md:w-auto'
+      onClick={onClick}
+      disabled={isLoading}
+    >
       Enroll for {formattedPrice(price)}
     </Button>
   );
