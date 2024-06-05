39fc59e47dccb5584f2623f7195712dce8aa6aad
j-mahapatra <jagannathm.official@gmail.com>
2024-06-05 22:01:24 +0530

    refactor: remove old test account info

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -4,16 +4,6 @@ export default function Page() {
   return (
     <div className='flex flex-col w-full items-center'>
       <SignIn />
-      <p className='text-xs z-50 text-center pt-5 pb-2 w-full'>
-        Use the following credentials for exploring with the admin view:
-      </p>
-      <p className='text-xs z-50 text-center p-1 w-full'>
-        Email address:
-        <em> jmahapatra.dev@gmail.com</em>
-      </p>
-      <p className='text-xs z-50 text-center p-1 w-full'>
-        Password:<em> jm_admin</em>
-      </p>
     </div>
   );
 }
